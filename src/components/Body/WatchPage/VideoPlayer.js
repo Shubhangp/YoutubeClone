c58b8bcc8604db10bca343a9a9efb82f0399e439
Shubhangp <shubhanggpandey@gmail.com>
2023-09-09 17:29:46 +0530

    First Commit

--- src/components/Body/WatchPage/VideoPlayer.js
+++ src/components/Body/WatchPage/VideoPlayer.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const VideoPlayer = ({videoId}) => {
   return (
-    <div>
+    <div className='mt-10 ml-3'>
       <iframe
-        width="560"
-        height="315"
+        width="700"
+        height="400"
         src={"https://www.youtube.com/embed/"+ videoId}
         title="YouTube video player"
         frameBorder="0"
